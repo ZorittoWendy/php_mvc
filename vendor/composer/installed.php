f05d7455874ed05f260a394ef3b5c6fb48ab38b0
ZorittoWendy <zorittoprogramador@gmail.com>
2025-03-22 15:24:29 -0300

    chore: adicionando config composer e autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'zoritto/mvc',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2e32b563d061f18764bed28e6cc2e13b826d0d80',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'zoritto/mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2e32b563d061f18764bed28e6cc2e13b826d0d80',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
