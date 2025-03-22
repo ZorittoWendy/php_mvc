c77ee10d02d01e21e6e3605b545207677645df35
ZorittoWendy <zorittoprogramador@gmail.com>
2025-03-22 16:05:15 -0300

    feat: deixando dinamico a pagina

--- app/Controller/Pages/Home.php
+++ app/Controller/Pages/Home.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Pages;
 
 use App\Utils\View;
+use App\Model\Entity\Organization;
 
 /* Metodo responsavel por retornar o conteudo (view) da nossa home 
 @return string */
@@ -11,11 +12,13 @@ class Home extends Page
 {
     public static function getHome()
     {
+        // Organização 
+        $obOrganization = new Organization;
         // VIEW DA HOME
         $content = View::render('pages/home', [
-            'name' => 'zoritto - github',
-            'description' => 'pagina do github: https://github.com/ZorittoWendy/php_mvc',
-            'site' => 'www.linkedin.com/in/wendeson-farias'
+            'name'         => $obOrganization->name,
+            'description'  => $obOrganization->description,
+            'site'         => $site,
         ]);
         //RETORNA A VIEW DA PAGINA 
         return parent::getPage('ZORITTO - Canal - HOME', $content);
---
+++ app/Model/Entity/Organization.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Model\Entity;
+
+class Organization
+{
+    /* ID da organization 
+    @var interger */
+    public $id = 1;
+
+    /*  Nome  da organização 
+    @var string */
+    public $name = 'Canal Zoritto-dev';
+
+    /* Site da organização 
+    @var string  */
+    public $site = 'https://github.com/ZorittoWendy/php_mvc';
+
+    /* descricação 
+    @var string */
+    public $description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, et. Sapiente odit laudantium maiores maxime. Est voluptas excepturi repudiandae earum dolorum, eos laborum illo totam accusamus quam enim nulla minima? çLorem ipsum dolor sit amet consectetur adipisicing elit. Harum molestiae quae dignissimos, tenetur reiciendis suscipit commodi odit repudiandae aliquid quas nostrum rerum dicta nihil illum officia distinctio in dolor! Tempore!
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, et. Sapiente odit laudantium maiores maxime. Est voluptas excepturi repudiandae earum dolorum, eos laborum illo totam accusamus quam enim nulla minima? çLorem ipsum dolor sit amet consectetur adipisicing elit. Harum molestiae quae dignissimos, tenetur reiciendis suscipit commodi odit repudiandae aliquid quas nostrum rerum dicta nihil illum officia distinctio in dolor! Tempore!';
+}
